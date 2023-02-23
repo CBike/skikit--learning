1988ed00722c8ac52613ae13ccdcc3ec04b35ef6
yangho.jeon <64768242+CBike@users.noreply.github.com>
2023-02-23 13:53:07 +0900

    study

--- iris_example.py
+++ iris_example.py
@@ -1,13 +1,8 @@
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
 iris_dataset = load_iris()
 
 
-print("iris_dataset의 키: \n{}".format(iris_dataset.keys()))
-# print(iris_dataset['DESCR'][:-1] + "\n...")
-print("타깃의 이름: {}".format(iris_dataset['target_names']))
-print("특성의 이름: \n{}".format(iris_dataset['feature_names']))
-print("data의 타입: {}".format(type(iris_dataset['data'])))
-print("data의 처음 다섯 행:\n{}".format(iris_dataset['data'][:5]))
-print("target의 타입: {}".format(type(iris_dataset['target'])))
-print("target의 크기: {}".format(iris_dataset['target'].shape))
-print("타깃:\n{}".format(iris_dataset['target']))
+X_train, X_test, y_train, y_test = train_test_split(
+iris_dataset['data'], iris_dataset['target'], random_state=0)
